e5805653242edd8b0576c4fd361156226156bacb
Melbettal <meryameelbettal@gmail.com>
2020-01-20 23:46:18 +0100

    done

--- Brief04/Code/res/js/script.js
+++ Brief04/Code/res/js/script.js
@@ -1,5 +1,14 @@
 window.onload = ()=>{
-    function myFunction(x) {
-        x.classList.toggle("change");
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > 300) {
+        $(".black").css("background" , "blue");
       }
+  
+      else{
+        $(".black").css("background" , "#333");  	
+      }
+    })
+  })
 }
